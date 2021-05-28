8c43b40c768b5dcf89a6c41b3bfc0b67da588191
Dmytro Kachan <83279757+DimitriyKachan@users.noreply.github.com>
2021-05-28 09:53:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import numpy as np
 A = np.array([[5.103, 0.293, 0.336, 0.270],
               [0.179, 4.912, 0.394, 0.375],
               [0.189, 0.321, 2.875, 0.216],
-              [0.317, 0.165, 0.386, 3.934]], dtype=float)
-b = np.array([0.745, 0.381, 0.480, 0.552])
+              [0.317, 0.165, 0.386, 3.934]], dtype=float) #matrix of coefficients near x
+b = np.array([0.745, 0.381, 0.480, 0.552]) #vector of "answers", numbers that are onthe other side of the equations
 
 
 def DRL(Matrix):
+  """Part of the DRL method to solve system of linear equations.
+ Particularly this function makes 3 matrix out of original.
+ Those 3 matrix are, matrix with values on the main diagonal(rest are zeros), 
+ values to the right side of main diagonal(rest are zeros), 
+ and values to the left side of main diagonal(rest are zeros)"""
     D = np.zeros((4, 4), dtype=float)
     R = np.zeros((4, 4), dtype=float)
     L = np.zeros((4, 4), dtype=float)
@@ -25,15 +30,19 @@ def DRL(Matrix):
 
 
 def reform_system(Matrix, vector):
+  """This is second part of DRL function, this function
+  computes new matrix and vector with the formulas below, 
+  then these values will be used in next function"""
     D, R, L = DRL(Matrix)
 
     B = -(np.linalg.inv(D).dot(L + R))
-    w, v = np.linalg.eigh(B)
     c = np.linalg.inv(D).dot(vector)
     return B, c
 
 
 def yakobi(Matrix, vector):
+  """This is plain yakobi method of solving linear systems, 
+  but we use new values of matrix and vector"""
     B, c = reform_system(Matrix, vector)
     x0 = [100, 100, 100, 100]
     eps = 0.00001
@@ -55,6 +64,7 @@ def yakobi(Matrix, vector):
 
 
 def check(Matrix, vector, answer):
+  #checking roots
     return vector - Matrix.dot(answer)
 
 
